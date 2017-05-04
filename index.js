d1b3a682a4c44fee79b39be34caae5aa034c0bf9
Steven Trefethen <steve@stevetrefethen.com>
2017-05-04 01:13:37 -0700

    Add pug template support for generating documentation.

--- index.js
+++ index.js
@@ -3,6 +3,29 @@ var request = require('superagent');
 var co = require('co');
 var prompt = require('co-prompt');
 var program = require('commander');
+const pug = require('pug');
+const fs = require('fs');
+const mkdirp = require('mkdirp');
+const path = require('path');
+
+function findObjectsAndMethods(paths) {
+  var objects = {};
+  for (var p in paths) {
+    var splitpath = p.split(path.sep);
+    if (splitpath.length == 2) {
+      objects[splitpath[1]] = paths[p];
+      continue;
+    }
+    if (!objects[splitpath[1]]) {
+      objects[splitpath[1]] = { methods: [] };
+    }
+    for (var i = 2; i < splitpath.length; i++) {
+      if (!splitpath[i].startsWith('{'))      
+        objects[splitpath[1]][splitpath[i]] = paths[p];
+    }
+  }
+  return objects;
+}
 
 program
   .version('0.0.1')
@@ -14,7 +37,25 @@ program
     request
       .get(swagger_url)
       .end(function (err, res) {
-        console.log(res.body);
+        console.log(JSON.stringify(findObjectsAndMethods(res.body.paths)));
+        fs.writeFile(program.path + '/index.html', pug.renderFile('templates/home.pug', res.body), () => { });
+        var seen = {}
+        for (var p in res.body.paths) {
+          var splitpath = p.split(path.sep);
+          var root = splitpath[1];
+          if (seen[root]) {
+            if (splitpath[2].startsWith('{'))
+              continue;
+            console.log(program.path + path.sep + root + path.sep + splitpath[2] + '.html');
+            fs.writeFile(program.path + path.sep + root + path.sep + splitpath[2] + '.html', pug.renderFile('templates/method.pug', { method: splitpath[2], info: res.body.paths[p] }), () => {});
+            continue;
+          } else {
+            seen[root] = true;
+            mkdirp(program.path + path.sep + root);
+            if (res.body.paths[path.sep + root])
+              fs.writeFile(program.path + path.sep + root + path.sep + 'index.html', pug.renderFile('templates/object.pug', { object: root, info: res.body.paths[path.sep + root] }), () => {});
+          }
+        }
       })
   })
   .parse(process.argv);
