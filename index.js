35527a35a0852f4ae4cbf3fec03bd3c694770c06
Steve Trefethen <strefethen@users.noreply.github.com>
2017-05-04 21:28:16 -0700

    Flesh out the basic content structure, include links to definitions

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const path = require('path');
 function findObjectsAndMethods(paths) {
   var objects = {};
   for (var p in paths) {
-    var splitpath = p.split(path.sep);
+    var splitpath = p.split('/');
     if (splitpath.length == 2) {
       objects[splitpath[1]] = paths[p];
       continue;
@@ -20,11 +20,31 @@ function findObjectsAndMethods(paths) {
       objects[splitpath[1]] = { methods: [] };
     }
     for (var i = 2; i < splitpath.length; i++) {
-      if (!splitpath[i].startsWith('{'))      
-        objects[splitpath[1]][splitpath[i]] = paths[p];
+      if (!splitpath[i].startsWith('{'))
+        if (!objects[splitpath[1]]['methods']) {
+          objects[splitpath[1]]['methods'] = { [splitpath[i]]: paths[p] };  
+        } else {
+          objects[splitpath[1]]['methods'][splitpath[i]] = paths[p];
+        }
     }
   }
   return objects;
+}
+
+function processSwagger(swagger, outpath) {
+  var objectsAndMethods = findObjectsAndMethods(swagger.paths);
+  fs.writeFileSync(outpath + '/index.html', pug.renderFile('templates/home.pug', { swagger: swagger, objects: objectsAndMethods }), () => { });
+  for (var key of Object.keys(objectsAndMethods)) {
+    mkdirp(outpath + path.sep + key);
+    fs.writeFileSync(outpath + path.sep + key + path.sep + 'index.html', pug.renderFile('templates/object.pug', { swagger: swagger, object: key, info: objectsAndMethods[key] }), () => {});
+    for (var method of Object.keys(objectsAndMethods[key].methods)) {
+      fs.writeFileSync(outpath + path.sep + key + path.sep + method + '.html', pug.renderFile('templates/method.pug', { swagger: swagger, parent: key, method: method, info: objectsAndMethods[key].methods[method] }), () => {});
+    }
+    mkdirp(outpath + path.sep + 'definitions');
+    for (var definition in swagger.definitions) {
+      fs.writeFileSync(outpath + '/definitions/' + definition + '.html', pug.renderFile('templates/definition.pug', { swagger: swagger, definition: definition, info: swagger.definitions[definition] }), () => { });
+    }
+  }
 }
 
 program
@@ -37,25 +57,8 @@ program
     request
       .get(swagger_url)
       .end(function (err, res) {
-        console.log(JSON.stringify(findObjectsAndMethods(res.body.paths)));
-        fs.writeFile(program.path + '/index.html', pug.renderFile('templates/home.pug', res.body), () => { });
-        var seen = {}
-        for (var p in res.body.paths) {
-          var splitpath = p.split(path.sep);
-          var root = splitpath[1];
-          if (seen[root]) {
-            if (splitpath[2].startsWith('{'))
-              continue;
-            console.log(program.path + path.sep + root + path.sep + splitpath[2] + '.html');
-            fs.writeFile(program.path + path.sep + root + path.sep + splitpath[2] + '.html', pug.renderFile('templates/method.pug', { method: splitpath[2], info: res.body.paths[p] }), () => {});
-            continue;
-          } else {
-            seen[root] = true;
-            mkdirp(program.path + path.sep + root);
-            if (res.body.paths[path.sep + root])
-              fs.writeFile(program.path + path.sep + root + path.sep + 'index.html', pug.renderFile('templates/object.pug', { object: root, info: res.body.paths[path.sep + root] }), () => {});
-          }
-        }
+        mkdirp(program.path);
+        processSwagger(res.body, program.path);
       })
   })
   .parse(process.argv);
